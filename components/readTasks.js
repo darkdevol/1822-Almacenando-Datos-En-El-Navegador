9da9151db9ac9457eb49e47cd18e96f7847426af
Axel <derfleraxel@gmail.com>
2022-12-06 14:38:29 -0300

    a

--- components/readTasks.js
+++ components/readTasks.js
@@ -1,12 +1,22 @@
 import { createTask } from "./addTask.js";
+import { uniqueDates, orderDates } from "../services/date.js";
+import dateElement from "./dateElement.js";
 
-export const readTasks = () => {
+
+export const displayTasks = () => {
     const list = document.querySelector('[data-list]');
-
     const TasksList = JSON.parse(localStorage.getItem("tasks")) || [];
-
-    TasksList.forEach((task) => {
-        list.appendChild(createTask(task));
-    })
-
+    const dates = uniqueDates(TasksList);
+    orderDates(dates);
+    dates.forEach( date => {
+        const dateMoment = moment(date, 'DD/MM/YYYY')
+        list.appendChild(dateElement(date))
+        TasksList.forEach((task) => {
+            const taskDate = moment(task.dateFormat, 'DD/MM/YYYY')
+            const diff = dateMoment.diff(taskDate)
+            if( diff === 0){
+                list.appendChild(createTask(task));
+            }
+        }); 
+    });
 };
